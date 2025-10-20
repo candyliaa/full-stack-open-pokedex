f7a532397e573789e7114bd9c533d2470ebaabfa
candyliaa <astronomy.dev@gmail.com>
2025-10-20 17:21:38 +0300

    break health check

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 5000 //eslint-disable-line
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('ok')
+  if (true) throw('error... ')
+  res.send(ok)
 })
 
 app.listen(PORT, () => {
